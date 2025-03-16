befba5a231f666a9b2055c98a25ab0386b6d5824
phuctoantin879 <thayphuctoan.10@gmail.com>
2025-03-16 23:54:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def convert():
             pass
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=False, host='0.0.0.0', port=port)
